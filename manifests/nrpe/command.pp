add8bc2627c5aebee10367d14fbaacccc7e1a149
Benjamin Sonntag <benjamin@octopuce.fr>
2015-03-26 14:20:48 +0100

    [fix] DRBD for Z2 & JOE

--- manifests/nrpe/command.pp
+++ manifests/nrpe/command.pp
@@ -8,18 +8,18 @@ define nagios::nrpe::command (
         fail ( "Either one of 'command_line' or 'source' must be given to nagios::nrpe::command." )
     }
 
-    if $nagios_nrpe_cfg_dir == '' {
+    if $nagios_nrpe_cfg_dir == '' and $nagios::nrpe::base::nagios_nrpe_cfgdir {
         $nagios_nrpe_cfgdir = $nagios::nrpe::base::nagios_nrpe_cfgdir
+    }
+    if $nagios_nrpe_cfg_dir == '' and $nagios::nrpe::base::nagios_nrpe_cfgdir == '' {
+        $nagios_nrpe_cfgdir = "/etc/nagios"
     }
 
     file { "$nagios_nrpe_cfgdir/nrpe.d/${name}_command.cfg":
                     ensure => $ensure,
                     mode => 644, owner => root, group => 0,
                     notify => Service['nagios-nrpe-server'],
-                    require => [ 
-		    	    File["$nagios_nrpe_cfgdir/nrpe.d"],
-			    Class["nagios::nrpe::base"]
-			    ]
+                    require => File["$nagios_nrpe_cfgdir/nrpe.d"],
     }
 
     case $source {
