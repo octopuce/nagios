06a8bb17517296abbfbb493c7535b67f121b6ae3
alban <alban@octopuce.fr>
2015-10-26 11:59:13 +0100

    [enh] Adds a timeout compliant  nrpe command

--- manifests/command/nrpe.pp
+++ manifests/command/nrpe.pp
@@ -11,4 +11,11 @@ class nagios::command::nrpe {
     'check_nrpe_1arg':
        command_line => '/usr/lib/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$'
   }
+
+  # this command runs a program $ARG1$ with timeout
+  nagios_command {
+    'check_nrpe_1arg_timeout':
+       command_line => '/usr/lib/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -t $ARG2$'
+  }
+
 }
