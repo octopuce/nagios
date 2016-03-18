80719568e14777580545414d70a99c0ed3b694e9
Benjamin Sonntag <benjamin@octopuce.fr>
2016-03-18 15:44:02 +0100

    adding monitoring-plugins-standard for jessie instead of nagios-plugins-standard

--- manifests/nrpe/linux.pp
+++ manifests/nrpe/linux.pp
@@ -1,7 +1,18 @@
 class nagios::nrpe::linux inherits nagios::nrpe::base {
 
+  if $lsbdistcodename == "jessie" {
+    
     package {
-        "nagios-plugins-standard": ensure => present;
+      "nagios-plugins-standard": ensure => purged;
+      "monitoring-plugins-standard": ensure => present;
     }
-
+    
+  } else {
+    
+    package {
+      "nagios-plugins-standard": ensure => present;
+    }
+  }
+  
 }
+
